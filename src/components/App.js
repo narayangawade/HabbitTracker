f1c65c41c812d44828f080f5693a183dc714bb47
narayangawade <117194242+narayangawade@users.noreply.github.com>
2023-07-10 19:23:45 +0530

    FinalCode
    habbit tracker react

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import WeekView from "./WeekView";
+import { Toaster } from "react-hot-toast";
+const App = () => {
+  
+  
+  return (
+    <>
+    <Toaster />
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/week-view" element={<WeekView/>}/>
+    </Routes>
+    </>
+  );
+};
+
+export default App;
